750aee23dde6320b46002382d559d50947c9fa5a
EduardOshurkov <95302939+EduardOshurkov@users.noreply.github.com>
2022-09-22 12:40:45 +0000

    Deploying to gh-pages from @ EduardOshurkov/goit-react-hw-08-phonebook@227396fa458c024ce63321edc31bbb851aec4c59 🚀

---
+++ static/js/795.31417ca9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[795],{1795:function(e,o,n){n.r(o);var h=n(184);o.default=function(){return(0,h.jsx)("div",{children:(0,h.jsx)("h2",{children:"Hello Home"})})}}}]);
+//# sourceMappingURL=795.31417ca9.chunk.js.map
